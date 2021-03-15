16b38ce9a966534fba413fe45ded181c144247d6
nalbam <me@nalbam.com>
2021-03-15 17:07:56 +0900

    v0.2.17

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,35 @@ REPOSITORY=${GITHUB_REPOSITORY}
 USERNAME=${USERNAME:-$GITHUB_ACTOR}
 REPONAME=$(echo "${REPOSITORY}" | cut -d'/' -f2)
 
-_error() {
-  echo -e "$1"
+command -v tput > /dev/null && TPUT=true
 
+_echo() {
+  if [ "${TPUT}" != "" ] && [ "$2" != "" ]; then
+    echo -e "$(tput setaf $2)$1$(tput sgr0)"
+  else
+    echo -e "$1"
+  fi
+}
+
+_result() {
+  echo
+  _echo "# $@" 4
+}
+
+_command() {
+  echo
+  _echo "$ $@" 3
+}
+
+_success() {
+  echo
+  _echo "+ $@" 2
+  exit 0
+}
+
+_error() {
+  echo
+  _echo "- $@" 1
   if [ "${LOOSE_ERROR}" == "true" ]; then
     exit 0
   else
@@ -63,21 +89,21 @@ _docker_tag() {
 }
 
 _docker_push() {
-  echo "docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}"
+  _command "docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}"
   docker build ${DOCKER_BUILD_ARGS} -t ${IMAGE_URI}:${TAG_NAME} -f ${DOCKERFILE} ${BUILD_PATH}
 
   _error_check
 
-  echo "docker push ${IMAGE_URI}:${TAG_NAME}"
+  _command "docker push ${IMAGE_URI}:${TAG_NAME}"
   docker push ${IMAGE_URI}:${TAG_NAME}
 
   _error_check
 
   if [ "${LATEST}" == "true" ]; then
-    echo "docker tag ${IMAGE_URI}:latest"
+    _command "docker tag ${IMAGE_URI}:latest"
     docker tag ${IMAGE_URI}:${TAG_NAME} ${IMAGE_URI}:latest
 
-    echo "docker push ${IMAGE_URI}:latest"
+    _command "docker push ${IMAGE_URI}:latest"
     docker push ${IMAGE_URI}:latest
   fi
 }
@@ -115,14 +141,14 @@ _docker_pre() {
 _docker() {
   _docker_pre
 
-  echo "docker login ${REGISTRY} -u ${USERNAME}"
+  _command "docker login ${REGISTRY} -u ${USERNAME}"
   echo ${PASSWORD} | docker login ${REGISTRY} -u ${USERNAME} --password-stdin
 
   _error_check
 
   _docker_push
 
-  echo "docker logout"
+  _command "docker logout"
   docker logout
 }
 
@@ -168,17 +194,17 @@ text
 EOF
 
   # https://docs.aws.amazon.com/cli/latest/reference/ecr/get-login.html
-  # echo "aws ecr get-login --no-include-email"
+  # _command "aws ecr get-login --no-include-email"
   # aws ecr get-login --no-include-email | sh
 
-  echo "aws ecr get-login-password ${AWS_ACCOUNT_ID} ${AWS_REGION}"
+  _command "aws ecr get-login-password ${AWS_ACCOUNT_ID} ${AWS_REGION}"
   aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/
 
   _error_check
 
   COUNT=$(aws ecr describe-repositories --output json | jq '.repositories[] | .repositoryName' | grep "\"${IMAGE_NAME}\"" | wc -l | xargs)
   if [ "x${COUNT}" == "x0" ]; then
-    echo "aws ecr create-repository ${IMAGE_NAME}"
+    _command "aws ecr create-repository ${IMAGE_NAME}"
     aws ecr create-repository --repository-name ${IMAGE_NAME} --image-tag-mutability ${IMAGE_TAG_MUTABILITY}
   fi
 
@@ -189,7 +215,7 @@ if [ -z "${CMD}" ]; then
   CMD="--docker"
 fi
 
-echo "[${CMD:2}] start..."
+_result "[${CMD:2}] start..."
 
 case "${CMD:2}" in
   docker)
